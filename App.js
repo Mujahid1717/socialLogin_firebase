b09e4d56200cba076bf02fd7d99ded7665ad606f
Mujahid1717 <mujahidakhtar2003@gmail.com>
2023-06-15 11:00:10 +0500

    applying social login using firebase

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View>
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
       <Text>App</Text>
     </View>
   )
